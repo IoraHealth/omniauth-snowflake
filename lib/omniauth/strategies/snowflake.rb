da2bdee10955f4bd2658d3387b8799e4474fbbcf
GrrrD <gerapl27@gmail.com>
2017-07-25 16:27:00 -0400

    use new me api and make url configurable

--- lib/omniauth/strategies/snowflake.rb
+++ lib/omniauth/strategies/snowflake.rb
@@ -5,8 +5,9 @@ module OmniAuth
   module Strategies
     class Icis < OmniAuth::Strategies::OAuth2
       option :client_options, { :site => 'https://snowflake.icisapp.com' }
+      option :info_url, '/api/v2/me.json'
 
-      uid { raw_info["uid"] }
+      uid { raw_info[raw_info.keys.first]["uid"] }
 
       info { raw_info }
 
@@ -21,7 +22,7 @@ module OmniAuth
       end
 
       def raw_info
-        @raw_info ||= access_token.get('/api/v1/me.json').parsed
+        @raw_info ||= access_token.get(options.info_url).parsed
       end
     end
   end
